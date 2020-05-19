49143f91fb0d042829168e9189bab6bc08a98d18
Alexander Frantsev <Alexander.Frantsev@sas.com>
2020-05-20 00:33:30 +0300

    Changes in title fields

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Tutorial(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    published = models.DateTimeField("Date Published")
+    published = models.DateTimeField("Date Published",default=datetime.now())
 
     def __str__(self):
         return self.title
