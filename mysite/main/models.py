9bc12a821df54b1d5256ee8f50f50dcb1e22d736
Alexander Frantsev <Alexander.Frantsev@sas.com>
2020-05-19 18:25:53 +0300

    New app: main

---
+++ mysite/main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Tutorial(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published = models.DateTimeField("Date Published")
+
+    def __str__(self):
+        return self.title
